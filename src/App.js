8b822a7138c5925074b309aba3c9325aaea2300e
m00re1 <mauricemoore100@gmail.com>
2024-06-15 20:18:07 +0200

    adding initial test detail page  +  All Test option on TestGroupDetail page  + added ability to link to dependent tests

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import AddTestPage from './pages/AddTest.js'
 import TestGroups from './pages/TestGroups.js'
 import HomePage from './pages/HomePage.js'
 import TestGroupDetail from './pages/TestGroupDetail.js'; // Import the new component
+import TestDetail from './pages/TestDetail.js'; // Import the new component
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import 'dayjs/locale/en-gb';
@@ -18,6 +19,7 @@ function App() {
           <Route path="/testgroups" element={<TestGroups/>} />
           <Route path="/addtest" element={<AddTestPage/>} />
           <Route path="/testgroup/:name/:date" element={<TestGroupDetail />} />
+          <Route path="/testdetail/:testId/:date" element={<TestDetail />} />
         </Routes>
       </Router>
     </LocalizationProvider>
